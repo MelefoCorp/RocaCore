2449cc048b864e0608bed0639cd2b7dae5d2c3a7
Melefo <melefo@melefo.xyz>
2021-12-09 11:11:50 +0100

    Moving List extension to enumerable interface

--- Roca.Core/Extensions/IEnumerableExtensions.cs
+++ Roca.Core/Extensions/IEnumerableExtensions.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace Roca.Core.Extensions
 {
-    public static class ListExtensions
+    public static class IEnumerableExtensions
     {
-        public static string? GetBestText(this List<TranslatedText> list) =>
+        public static string? GetBestText(this IEnumerable<TranslatedText> list) =>
             list.SingleOrDefault(x => x.Approved)?.Content ??
             list.OrderByDescending(x => x.Upvotes.Count - x.Downvotes.Count).FirstOrDefault()?.Content;
     }
